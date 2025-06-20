4f7c47a3911c04c725dfe05df6fb456819b9cee8
pn481 <pntandane72@gmail.com>
2025-06-20 12:02:13 -0700

    coomit main

--- hardware/vite.config.js
+++ hardware/vite.config.js
@@ -1,17 +1,16 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+
 export default defineConfig({
   plugins: [
     react(),
    tailwindcss(), 
-  base: '/', // Important for routing
-  ]
+  ],
+  base: '/,' // Important for Vercel routing
 });
 
 
